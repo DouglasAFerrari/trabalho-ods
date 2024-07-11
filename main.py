792d9b125c0e9fc89de3db0bf9677a4a288b7ab8
DouglasAFerrari <douglasangeloferrari@gmail.com>
2024-07-11 11:19:06 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+def main():
+    # Solicita o nome da pessoa
+    nome = input("Olá! Qual é o seu nome? ")
+
+    # Mensagem de boas-vindas personalizada
+    print(f"\nOlá, {
+          nome}! Vamos falar sobre ação climática e o ODS 13: Ação contra a mudança global do clima.\n")
+
+    # Mensagens sobre a importância da ação climática
+    print("A ação climática é crucial para o futuro do nosso planeta.")
+    print("Pequenas ações individuais podem fazer uma grande diferença.")
+    print("Reduzir o consumo de energia e usar fontes renováveis são passos importantes.")
+    print("Preservar nossas florestas e promover práticas sustentáveis também são essenciais.\n")
+
+    # Conclusão
+    print("Vamos juntos fazer a diferença! Cada um de nós pode contribuir para um futuro mais sustentável.\n")
+
+
+if __name__ == "__main__":
+    main()
